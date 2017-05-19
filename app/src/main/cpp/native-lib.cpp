fc7497d3383e3f00c23b21539aa54e9fd24790db
zhaitao <370985116@qq.com>
2017-05-19 19:24:21 +0800

    no message

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,14 @@
 #include <jni.h>
 #include <string>
 
-extern "C"
+// 按照C语言进行编译 保证C++能够调用
+extern "C" {
+#include <libavcodec/avcodec.h>
+
 JNIEXPORT jstring JNICALL
 Java_io_seniorzhai_cmake_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(avcodec_configuration());
+}
 }
