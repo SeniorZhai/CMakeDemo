6cda9d4585221a3853b375212aeca36dddffbcde
zhaitao <370985116@qq.com>
2017-05-22 18:14:52 +0800

    ffmpeg info

--- app/src/main/java/io/seniorzhai/cmake/MainActivity.java
+++ app/src/main/java/io/seniorzhai/cmake/MainActivity.java
@@ -2,6 +2,7 @@ package io.seniorzhai.cmake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,19 +12,47 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv = (TextView) findViewById(R.id.sample_text);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String avformatinfo();
+
+    public native String urlprotocolinfo();
+
+    private native String avcodecinfo();
+
+    private native String avconfiguration();
+
+    private native String avfilter();
+
+    public void url(View view) {
+        tv.setText(urlprotocolinfo());
+    }
+
+    public void format(View view) {
+        tv.setText(avformatinfo());
+    }
+
+
+    public void avcodec(View view) {
+        tv.setText(avcodecinfo());
+    }
+
+    public void configuration(View view) {
+        tv.setText(avconfiguration());
+    }
+
+    public void filter(View view) {
+        tv.setText(avfilter());
+    }
 }
